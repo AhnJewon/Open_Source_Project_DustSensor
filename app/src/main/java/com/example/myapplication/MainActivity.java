e560790691de0b3e87142114cc7c8daa20b20bf9
AhnJewon <inno6734@gmail.com>
2024-03-21 20:48:26 +0900

    스캔기능 활성화

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.RequiresApi;
@@ -33,89 +35,122 @@ public class MainActivity extends AppCompatActivity {
 
     private Retrofit retrofit;
 
-    @RequiresApi(api = Build.VERSION_CODES.S)
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
         }
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1000);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, 1000);
         }
 
 
-
-        if(!blead.isEnabled())
+        if (!blead.isEnabled())
             blead.enable();
-
-        //blead.startLeScan(scancallback_le);
+        Log.e("ble", String.valueOf(blead.isEnabled()));
+        Log.e("ble", String.valueOf(blead));
+//        Button bt = findViewById(R.id.bt);
+//        bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+        blead.startLeScan(scancallback_le);
         //blead.stopLeScan(scancallback_le);
 
 
-
     }
 
 
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
 
-        blead.startLeScan(scancallback_le);
 
     }
 
-    protected void onResume(){
-        super.onResume();
+//    protected void onResume() {
+//        super.onResume();
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//
+//        blead.startLeScan(scancallback_le);
+//
+//
+//        Gson gson = new GsonBuilder().setLenient().create();
+//        retrofit = new Retrofit.Builder()
+//                .baseUrl("http://203.255.81.72:10021/")
+//                .addConverterFactory(ScalarsConverterFactory.create())
+//                .addConverterFactory(GsonConverterFactory.create(gson))
+//                .build();
+//
+//        comm_data service = retrofit.create(comm_data.class);
+//
+//        Call<String> call = null;
+//        call = service.post("ONE", data);
+//
+//        call.enqueue(new Callback<String>() {
+//            @Override
+//            public void onResponse(Call<String> call, Response<String> response) {
+//                Log.e("test", response.body().toString());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<String> call, Throwable t) {
+//
+//            }
+//
+//        });
+//
+//    }
 
-
-
-
-    }
-
-    protected void onStop(){
-        super.onStop();
-        blead.stopLeScan(scancallback_le);
-    }
+//    protected void onStop() {
+//        super.onStop();
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//
+//        blead.stopLeScan(scancallback_le);
+//    }
 
     private BluetoothAdapter.LeScanCallback scancallback_le = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            Log.e("ble", "스캔됨");
 
             String MacAdd = device.getAddress();
             String data = bytearrayToHex(scanRecord);
+            Log.e("password", data);
 
-            Gson gson = new GsonBuilder().setLenient().create();
-            retrofit = new Retrofit.Builder()
-                    .baseUrl("http:203.255.81.72:10021/")
-                    .addConverterFactory(ScalarsConverterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
 
-            comm_data service = retrofit.create(comm_data.class);
-
-            Call<String> call = null;
-            call = service.get("AHN", "test");
-
-            call.enqueue(new Callback<String>(){
-                @Override
-                public void onResponse(Call<String> call, Response<String> response) {
-                    Log.e("test", response.body().toString());
-                }
-
-                @Override
-                public void onFailure(Call<String> call, Throwable t) {
-
-                }
-
-            });
 
         }
     };
