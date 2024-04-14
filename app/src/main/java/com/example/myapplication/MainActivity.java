ae505af95bb953be573c0bb55619f0a8cff7f659
AhnJewon <inno6734@gmail.com>
2024-04-14 18:30:06 +0900

    add byte decoding func form LCU

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,6 +24,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.Locale;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -122,14 +124,80 @@ public class MainActivity extends AppCompatActivity {
             sb.append(String.format("%02x", b));
 
         String pass = sb.toString();
+        Log.i("Pass", pass);
 
-        int index = pass.indexOf("998899");
+        //바뀐 부분 127~182번줄
+        //timeotp, sensingtime, sensordata는 NULL이 아니게 설정해야 join할 때 오류 안남
+        String timeotp="", sensingtime="", sensorone, sensortwo, sensorthree, sensordata="";
+
+        int index = pass.indexOf("f0f0");
         if(index != -1) {
-            pass = pass.substring(index + 6);
-            index = pass.indexOf("998899");
-            pass = pass.substring(0, index);
-            Log.i("password", pass);
+            pass = pass.substring(index + 4);
+            //TimeOTP = pass.substring(0, 6);
+            //bool을 1로 해서 0 없이 나오게 하기 (%d)
+            timeotp = hexToDec(pass.substring(0, 6),1);
+            Log.i("TimeOTP", timeotp);
         }
+
+
+        index = pass.indexOf("9999");
+        if(index != -1) {
+            pass = pass.substring(index+4);
+            //sensingtime = pass.substring(0, 10);
+            //bool을 0으로 해서 앞에 0 붙여서 나오게 하기 (%02d)
+            sensingtime = hexToDec(pass.substring(0,10),0);
+            Log.i("SensingTime", sensingtime);
+        }
+
+
+        index = pass.indexOf("fd");
+        if(index != -1) {
+            pass = pass.substring(index+2);
+            //sensorone = pass.substring(0, 2);
+            //sensortwo = pass.substring(2, 4);
+            //sensorthree = pass.substring(4, 6);
+            //bool을 1로 해서 0 없이 나오게 하기 (%d)
+            sensorone = hexToDec(pass.substring(0,2),1);
+            sensortwo = hexToDec(pass.substring(2,4),1);
+            sensorthree = hexToDec(pass.substring(4,6),1);
+            //join으로 /넣어서 합치기
+            sensordata = String.join("/", sensorone, sensortwo, sensorthree);
+
+            Log.i("SensorOne", sensorone);
+            Log.i("SensorTwo", sensortwo);
+            Log.i("SensorThree", sensorthree);
+            Log.i("sensordata: ", sensordata);
+        }
+
+//        int index = pass.indexOf("998899");
+//        if(index != -1) {
+//            pass = pass.substring(index + 6);
+//            index = pass.indexOf("998899");
+//            pass = pass.substring(0, index);
+//            Log.i("password", pass);
+//        }
+
+        //,로 join
+        pass = String.join(", ", timeotp, sensingtime, sensordata);
+
+        return pass;
+    }
+
+
+
+
+    private String hexToDec(String scanRecord, int bool){
+        StringBuilder sb = new StringBuilder(scanRecord.length() * 2);
+
+        for(int i = 0; i<scanRecord.length(); i+=2){
+            String hex = scanRecord.substring(i,i+2);
+            int decimal = Integer.parseInt(hex,16);
+
+            if(bool == 1) sb.append(String.format(Locale.US, "%d", decimal));
+            else sb.append(String.format(Locale.US, "%02d", decimal));
+        }
+
+        String pass = sb.toString();
 
         return pass;
     }
